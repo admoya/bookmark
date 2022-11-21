94e74ffb3196cb35f68ac15f857a8df8d153ce2c
Adrian Moya <amoya6@gatech.edu>
2022-11-21 03:45:57 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const books = [
+    {
+      title: 'test1',
+      page: 34,
+    },
+    {
+      title: 'test2',
+      page: 36,
+    }
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={{fontSize: 48, position: 'absolute', top: 50}}>Bookmark</Text>
+      <ScrollView>
+        {books.map(({ title, page }) => (
+          <View key={title} style={styles.listItem}>
+            <Text>{title}: {page}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +30,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 150,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listItem: {
+    padding: 10,
+    height: 44,
   },
 });
